353ba9fd459bb2d126ef1512b836dcb59e26743e
Renatolbn <renatolbn@gmail.com>
2026-03-07 16:51:55 -0300

    input categoria tipo select

--- LogicLab_js/server.js
+++ LogicLab_js/server.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
-import fs from "fs"; // 👈 Importar o módulo fs
+import fs from "fs"; // O módulo File System do Node.js permite ler e escrever arquivos no computador.
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
 
 const app = express();
-const ARQUIVO = "exercicios.json"; // 👈 Nome do arquivo
+const ARQUIVO = "exercicios.json"; 
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(".")); //Isso faz o Express servir os arquivos estáticos do diretório atual.
 
 // Funções auxiliares para ler e salvar
 function lerExercicios() {
@@ -19,10 +22,49 @@ function salvarExercicios(exercicios) {
   fs.writeFileSync(ARQUIVO, JSON.stringify(exercicios, null, 2));
 }
 
+/**
+ * @swagger
+ * /exercicios:
+ *   get:
+ *     summary: Lista todos os exercicios
+ *     responses:
+ *       200:
+ *         description: Retornado com sucesso
+ */
+
 // GET (criar futuramente filtro de categoria dos exercícios)
 app.get("/exercicios", (req, res) => {
   res.json(lerExercicios());
 });
+
+/**
+ * @swagger
+ * /exercicios:
+ *   post:
+ *     summary: Cria um novo exercicio
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               id:
+ *                 type: string
+ *               title:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *               description:
+ *                 type: string
+ *               codigo:
+ *                 type: string
+ *               explicacao:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Criado com sucesso
+ */
 
 // POST
 app.post("/exercicios", (req, res) => {
@@ -31,6 +73,24 @@ app.post("/exercicios", (req, res) => {
   salvarExercicios(exercicios);
   res.json({ ok: true });
 });
+
+/**
+ * @swagger
+ * /exercicios/{id}:
+ *   delete:
+ *     summary: Deleta um exercicio
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Deletado com sucesso
+ *       404:
+ *         description: Nao encontrado
+ */
 
 // DELETE
 app.delete("/exercicios/:id", (req, res) => {
@@ -47,6 +107,24 @@ app.delete("/exercicios/:id", (req, res) => {
   res.json({ ok: true });
 });
 
+/**
+ * @swagger
+ * /exercicios/{id}:
+ *   put:
+ *     summary: Edita um exercicio
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Atualizado com sucesso
+ *       404:
+ *         description: Nao encontrado
+ */
+
 // PUT
 app.put("/exercicios/:id", (req, res) => {
   const { id } = req.params;
@@ -60,6 +138,28 @@ app.put("/exercicios/:id", (req, res) => {
   exercicios[index] = req.body;
   salvarExercicios(exercicios);
   res.json({ ok: true });
+});
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "LogicLab API",
+      version: "1.0.0",
+      description: "API para gerenciar exercícios de programação",
+    },
+  },
+  apis: ["./server.js"], // 👈 onde estão os comentários da documentação
+};
+
+const swaggerDocs = swaggerJsdoc(swaggerOptions);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "LogicLab API rodando!",
+    docs: "http://localhost:3000/docs",
+  });
 });
 
 app.listen(3000, () => {
